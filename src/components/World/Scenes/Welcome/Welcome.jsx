b002cf26846416d1c228f6551c22c78fc74096d3
Carlos Hernandez <carlos.hernandez.agudelo@correounivalle.edu.co>
2023-10-20 21:59:17 -0500

    The milk model was added

--- src/components/World/Scenes/Welcome/Welcome.jsx
+++ src/components/World/Scenes/Welcome/Welcome.jsx
@@ -8,6 +8,7 @@ import { useRenderStore } from '../../../../store/storeWorld';
 import Bee from './Models/Characters/Bee';
 import Robot from './Models/Characters/Robot';
 import { Arrow } from './Models/Decoration/Arrow';
+import { Milk } from './Models/Decoration/Milk';
 import Room from './Models/Decoration/Room';
 import { Setup } from './Models/Decoration/Setup';
 import { Shelf } from './Models/Decoration/Shelf';
@@ -21,6 +22,7 @@ import WelcomeText from './Text/WelcomeText';
 
 const Welcome = () => {
   const textRef = useRef();
+  const milkRef = useRef();
   const [play, setPlay] = useState(false);
   const [hitSound] = useState(() => new Audio('/assets/sounds/hitBall.wav'));
 
@@ -36,8 +38,6 @@ const Welcome = () => {
     textRef.current.sleep();
   }, []);
 
-  
-
   const onHandleText = () => {
     textRef.current.wakeUp();
     textRef.current.applyImpulse({ x: 0, y: 2, z: 0 }, true);
@@ -47,10 +47,13 @@ const Welcome = () => {
 
   const onHandleRobot = () => {};
 
+  const handleMilk = () => {
+    milkRef.current.applyImpulse({ x: 0, y: 1, z: 0 }, true);
+  };
+
   return (
     <>
       <Room />
-
       {/* <RigidBody
         ref={textRef}
         colliders={'ball'}
@@ -67,7 +70,6 @@ const Welcome = () => {
           <meshStandardMaterial color="red" />
         </mesh>
       </RigidBody> */}
-
       {/* Characters*/}
       <Robot
         rotation={[0, -Math.PI * 0.15, 0]}
@@ -76,37 +78,31 @@ const Welcome = () => {
         onClick={onHandleRobot}
       />
       <Bee position={[3.3, 1.3, -3.2]} rotationY={-Math.PI * 0.15} scale={1} />
-
       {/* Decoration */}
-
-        colliders={'ball'}
+      colliders={'ball'}
       <RigidBody colliders={'cuboid'} gravityScale={0} type="fixed">
-
-      <Shelf position={[5.3, 0.5, -3]} scale={0.6} />
-</RigidBody>
-
+        <Shelf position={[5.3, 0.5, -3]} scale={0.6} />
+      </RigidBody>
       <WindowWall
         position={[2, -2, -3.7]}
         scale={0.039}
         rotation-y={-Math.PI / 2}
       />
-
       <Cube position={[5.4, 0.9, -2.2]} scale={0.5} />
-
       <Cone position={[5.4, 1, -3]} scale={0.6} />
-
       <Sign
         position={[1, -2, -2.5]}
         scale={1.4}
         rotation-y={Math.PI * 0.15}
       ></Sign>
-
-      <Setup
+      <RigidBody
+        colliders={'cuboid'}
         position={[1.7, -1.8, 35]}
         rotation={[0, toRadians(90), 0]}
         scale={2.5}
-      />
-
+      >
+        <Setup />
+      </RigidBody>
       <RigidBody
         ref={textRef}
         colliders={'cuboid'}
@@ -115,18 +111,26 @@ const Welcome = () => {
         size={0.6}
         onCollisionEnter={() => setPlay(!play)}
         onCollisionLeave={() => setPlay(!play)}
-
       >
         <WelcomeText onClick={onHandleText} />
       </RigidBody>
-
       <InitText position={[0, 2, -8]} />
-
       <Arrow
         position={[-5.65, 0.8, 1.5]}
         rotation-z={toRadians(90)}
         scale={0.2}
       />
+      {/* <Among position={[0,0,0]} /> */}
+      <RigidBody
+        ref={milkRef}
+        // onClick={() => handleMilk()}
+        colliders={'cuboid'}
+        position={[-4, 1.5, 1]}
+        rotation-y={toRadians(90)}
+        scale={0.2}
+      >
+        <Milk handleMilk={handleMilk} />
+      </RigidBody>
     </>
   );
 };
